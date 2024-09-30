6efe0085eb192d5dd588d4f61dc9410cd66765c7
Angel Andrade <angelomar.andradelinares@gmail.com>
2024-09-29 23:24:22 -0600

    Se corrigen algunos errores pero el codigo aun no es funcional

--- filtro.js
+++ filtro.js
@@ -8,7 +8,10 @@ const productos = [
   {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
+const li = document.getElementById("lista-de-productos")
+// Se intercambia getElementsByName por un getElementById
+
+
 const $i = document.querySelector('.input');
 
 for (let i = 0; i < productos.length; i++) {
@@ -27,7 +30,7 @@ for (let i = 0; i < productos.length; i++) {
 
   li.appendChild(d)
 }
-
+// displayProductos es una function no definida
 displayProductos(productos)
 const botonDeFiltro = document.querySelector("button");
 
